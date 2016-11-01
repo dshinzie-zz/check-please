3fb5503999593331f7c4571358ef264587f021a6
Alisher Sadikov <alishersadikov@gmailcom>
2016-11-01 17:33:44 -0600

    update server login test

--- spec/features/servers/server_logs_in_spec.rb
+++ spec/features/servers/server_logs_in_spec.rb
@@ -17,15 +17,37 @@ describe "server logs in" do
     expect(path).to eq("/login")
   end
 
-  scenario "server sees the form to login and logs in" do
+  scenario "server sees the form to login and enters the right login credentials" do
     visit root_path
     click_on "Login"
 
-    fill_in "server[username]", with: server.usernmae
+    fill_in "server[username]", with: server.username
     fill_in "server[password]", with: server.password
     click_button "Login"
 
     expect(current_path).to eq(/dashboard)
     expect(page).to have_content("Logged in as #{server.usernmae}")
+  end
+
+  scenario "server sees the form to login and enters wrong password" do
+    visit root_path
+    click_on "Login"
+
+    fill_in "server[username]", with: server.username
+    fill_in "server[password]", with: "wrong pass"
+    click_button "Login"
+
+    expect(flash[:failure]).to match(/Wrong username! .*/)
+  end
+
+  scenario "server sees the form to login and enters wrong username" do
+    visit root_path
+    click_on "Login"
+
+    fill_in "server[username]", with: "wrong username"
+    fill_in "server[password]", with: server.password
+    click_button "Login"
+
+    expect(flash[:failure]).to match(/Wrong password! .*/)
   end
 end

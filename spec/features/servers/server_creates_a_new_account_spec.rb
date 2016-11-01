b07337cc0a2261d23628567b76dc798c63f36ecf
Alisher Sadikov <alishersadikov@gmailcom>
2016-11-01 17:53:40 -0600

    add sad paths to server account creation test

--- spec/features/servers/server_creates_a_new_account_spec.rb
+++ spec/features/servers/server_creates_a_new_account_spec.rb
@@ -4,15 +4,15 @@ describe "server creates a new account" do
 
   @server = Server.new(name: "Jeff", username: "j3", password: "321clap")
 
-  scenario "server goes to the login page" do
+  scenario "server successfully creates a new account" do
     visit login_path
 
     click_on "Create Account"
 
-    fill_in "server[name]", with @server.name
-    fill_in "server[username]", with @server.username
-    fill_in "server[password]", with @server.password
-    fill_in "server[password_confirmation]", with @server.password
+    fill_in "server[name]", with: @server.name
+    fill_in "server[username]", with: @server.username
+    fill_in "server[password]", with: @server.password
+    fill_in "server[password_confirmation]", with: @server.password
     click_on "Create"
 
     expect(current_path).to eq(/dashboard)
@@ -20,5 +20,46 @@ describe "server creates a new account" do
     expect(page).to have_content("Name: #{@server.name}")
     expect(page).to_not have_content("Login")
     expect(page).to have_content("Logout")
+  end
+
+  scenario "server cannot create an account without a password" do
+    visit login_path
+
+    click_on "Create Account"
+
+    fill_in "server[name]", with: @server.name
+    fill_in "server[username]", with: @server.username
+    click_on "Create"
+
+    expect(flash[:failure]).to match(/Please add a password! .*/)
+  end
+
+  scenario "server cannot create an account without confirming password" do
+    visit login_path
+
+    click_on "Create Account"
+
+    fill_in "server[name]", with: @server.name
+    fill_in "server[username]", with: @server.username
+    fill_in "server[password]", with: @server.password
+
+    click_on "Create"
+
+    expect(flash[:failure]).to match(/Please confirm your password! .*/)
+  end
+
+  scenario "server sees an error if they confirm wrong password" do
+    visit login_path
+
+    click_on "Create Account"
+
+    fill_in "server[name]", with: @server.name
+    fill_in "server[username]", with: @server.username
+    fill_in "server[password]", with: @server.password
+    fill_in "server[password_confirmation]", with: "1234"
+
+    click_on "Create"
+
+    expect(flash[:failure]).to match(/Passwords must match! .*/)
   end
 end

40c41df73a9bde73c2ddfb392be882a575006e60
Vido <vidoseaver@gmail.com>
2016-11-01 11:32:25 -0600

    add nav bar and test for categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'categories/index'
-
   get "/menu"  => "categories#index"
+  resources :categories, only: [:show]
   resources :servers
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- spec/features/User_can_see_categories_spec.rb
+++ spec/features/User_can_see_categories_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature "user see categories across the top " do
   it "displays two categories across the top" do
-    category_one = Category.create(name:"appitizers")
+    category_one = Category.create(name:"appetizers")
     category_two = Category.create(name:"beverages")
 
     visit menu_path

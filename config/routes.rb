1b096425bb4399b01246ac15ed02de5deb721818
Vido <vidoseaver@gmail.com>
2016-11-01 10:54:00 -0600

    fix routes and add custom menu route to categories index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get "/menu"  => "categories#index"
   resources :servers
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

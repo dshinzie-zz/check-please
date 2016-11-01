cf4ef0f0511db29419535d68af2d6c3747ead5b4
Vido <vidoseaver@gmail.com>
2016-11-01 15:17:39 -0600

    create seed file

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true
 
-  belongs_to :category, optional: true
+  belongs_to :category, optional: true, dependent: :destroy
   has_many :table_items
   has_many :tables, through: :table_items
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   resources :items, only: [:index]
   get "/menu"  => "categories#index"
-  get "/:category_name" => "categories#show", as: "category_name"
+  root "categories#index"
 
   resources :servers
+  get "/:category_name" => "categories#show", as: "category_name"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

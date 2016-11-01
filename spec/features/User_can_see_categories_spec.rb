df8e409333858ed42c7606e298996e16a67f5e3b
Vido <vidoseaver@gmail.com>
2016-11-01 10:56:36 -0600

    add gems binding of caller and shoulda matchers

--- spec/features/User_can_see_categories_spec.rb
+++ spec/features/User_can_see_categories_spec.rb
@@ -7,6 +7,7 @@ RSpec.feature "user see categories across the top " do
 
     visit menu_path
 
+
     expect(page).to have_content(category_one.name)
     expect(page).to have_content(category_two.name)
 

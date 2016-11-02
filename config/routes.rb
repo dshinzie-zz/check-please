6b036ebddc4d9d3e4085b545ceb1f15284a5c477
Noah Berman <noah@noahberman.org>
2016-11-02 11:24:00 -0600

    add session routes and session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
   post '/orders' => 'orders#create', as: "create_order"
   get "/order"  => "orders#index", as: "orders"
   delete "/order" => "orders#destroy", as: "order"

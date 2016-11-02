bb16fade05476b95098dfab77e18fa62e23e0c1a
Noah Berman <noah@noahberman.org>
2016-11-02 11:51:05 -0600

    add routes for new server and dashboard page plus controller actions

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -4,10 +4,17 @@ class ServersController < ApplicationController
   end
 
   def new
+    @server = Server.new
   end
 
   def create
-    @server = Server.new()
+    @server = Server.new(server_params)
+    if @server.save
+      flash[:success] = "Successfully created a new account!"
+      redirect_to dashboard_path(@server)
+    else
+      render :new
+    end
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-
+  
   post '/orders' => 'orders#create', as: "create_order"
   get "/order"  => "orders#index", as: "orders"
   delete "/order" => "orders#destroy", as: "order"
   resources :items, only: [:index]
-  resources :servers
-
+  resources :servers, except: [:show]
+  
+  get "/dashboard" => "server#show", as: "dashboard"
 
   get "/menu"  => "categories#index", as: "menu"
   root "categories#index"

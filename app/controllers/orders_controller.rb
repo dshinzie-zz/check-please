0e8232f9f982ad3793e7e886d927fd0e435ae9cc
Vido <vidoseaver@gmail.com>
2016-11-04 11:23:53 -0600

    dan sorry

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,17 +1,17 @@
 class OrdersController < ApplicationController
-  
+
   def index
     @server = Server.find(session[:server_id])
     @orders = @server.orders.all
   end
-  
+
   def show
     @order = Order.find(params[:id])
   end
 
   def create
     server = Server.find(server_id)
-    order = Order.new(server_id: server.id, total: @ticket.total, paid?: false )
+    order = Order.new(server_id: server.id, total: @ticket.total )
     if order.save
       create_order_items(order.id)
       redirect_to orders_path
--- spec/features/orders/User_can_see_order_status_spec.rb
+++ spec/features/orders/User_can_see_order_status_spec.rb
@@ -7,4 +7,29 @@ RSpec.describe "Orders status" do
 
     expect(order.status).to eq("ordered")
   end
+
+  it "order status can be updated" do
+    server = create(:server)
+    order = Order.new(server: server, total: 5.00, status: "completed")
+
+    expect(order.status).to eq("completed")
+  end
+
+  it "order status is only valid if from list" do
+    server = create(:server)
+
+    expect{Order.new(server: server, total: 5.00, status: "asdf")}.to raise_error(ArgumentError)
+  end
+end
+
+feature "order status" do
+  it "user can see order status on the order show page" do
+    server = create(:server)
+    item_1, item_2 = create_list(:item, 2)
+    order = Order.new(server: server, total:(item_1.price+item_2.price))
+    order_item_1 = OrderItem.new(item: item_1, order: order)
+    order_item_2 = OrderItem.new(item: item_2, order: order)
+
+    
+  end
 end

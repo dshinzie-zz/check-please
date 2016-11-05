7972707208c85aaa6f235954244bf95bcc1a63d7
Noah Berman <noah@noahberman.org>
2016-11-05 12:25:02 -0600

    downcase category names in params

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,12 @@
 class CategoriesController < ApplicationController
+  
   def index
       @categories = Category.all
   end
 
   def show
-    @category = Category.find_by(name: params[:category_name].capitalize)
+    @category = Category.find_by(name: params[:category_name].capitalize) || Category.find_by(name: params[:category_name])
     @items = @category.items
   end
+  
 end

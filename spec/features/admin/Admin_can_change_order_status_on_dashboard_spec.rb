396154caa217fec01d4f3490b04ffaf2f5ae69b2
Noah Berman <noah@noahberman.org>
2016-11-06 10:58:42 -0700

    add codeclimate patch file

--- spec/features/admin/Admin_can_change_order_status_on_dashboard_spec.rb
+++ spec/features/admin/Admin_can_change_order_status_on_dashboard_spec.rb
@@ -1,4 +1,4 @@
-require "rails-helper"
+require "rails_helper"
 
 RSpec.feature "admin can change the order status of orders from the dashboard" do
   it "admin can change" do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,7 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
-require "pry"
+require 'pry'
 
 RSpec.configure do |config|
 

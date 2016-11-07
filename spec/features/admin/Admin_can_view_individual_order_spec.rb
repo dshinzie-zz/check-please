00048f621acfe5d8edf17eca7b8f35b6cd079735
Vido <vidoseaver@gmail.com>
2016-11-07 13:14:54 -0700

    test showing order shows created at time

---
+++ spec/features/admin/Admin_can_view_individual_order_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "admin views the individual order" do
+  before :each do
+    @admin = create(:admin)
+    @server = create(:server)
+    @item_1, @item_2 = create_list(:item_with_category, 2)
+    @order = Order.create(server:@server, total:(@item_1.price + @item_2.price))
+
+    page.set_rack_session(server_id: @admin.id)
+
+    visit admin_dashboard_path
+  end
+  it "they can see order date and time" do
+
+    click_link "#{@order.id}"
+
+    expect(current_path).to eq(get_order_path(@order.id))
+    expect(page).to have_content(@order.created_at)
+  end
+end

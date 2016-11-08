959bb10dee929e68f466dfb73f84fddceee693d7
Noah Berman <noah@noahberman.org>
2016-11-08 11:11:16 -0700

    paperclip image upload config

--- Gemfile
+++ Gemfile
@@ -3,8 +3,10 @@ source 'https://rubygems.org'
 ruby '2.3.1'
 
 gem 'rb-readline'
-
+gem 'paperclip'
+gem 'aws-sdk', '~> 2.3'
 gem 'bootstrap-sass', '~>3.3.7'
+gem 'figaro'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 # Use postgresql as the database for Active Record
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,13 @@ class Item < ApplicationRecord
   belongs_to :category, optional: true, dependent: :destroy
   has_many :ticket_items
   has_many :tickets, through: :ticket_items
+  
+  has_attached_file :avatar, styles: {
+   thumb: '100x100>',
+   square: '200x200#',
+   medium: '300x300>'
+ }
+
+ # Validate the attached image is image/jpg, image/png, etc
+ validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end

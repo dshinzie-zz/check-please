c4c006ac3e0ad03bd4e6aa23408e302b50011c35
Daniel Shin <shin.daniel.d@gmail.com>
2016-11-08 14:43:24 -0700

    refactored sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,10 @@
 class SessionsController < ApplicationController
 
-  def new
-  end
-
   def create
     @server = Server.find_by(username: params[:username])
-    if @server
-      if @server.authenticate(params[:password])
-        flash[:success] = "Successfully logged in!"
-        session[:server_id] = @server.id
 
-        if @server.admin?
-          redirect_to admin_dashboard_path
-        else
-          redirect_to dashboard_path
-        end
-        
-      else
-        flash[:failure] = "Password incorrect!"
-        render :new
-      end
+    if @server
+      authenticate_server
     else
       flash[:failure] = "Username incorrect!"
       render :new
@@ -31,4 +16,26 @@ class SessionsController < ApplicationController
     flash[:success] = "Successfully logged out!"
     redirect_to login_path
   end
+
+  private
+
+  def authenticate_server
+    if @server.authenticate(params[:password])
+      flash[:success] = "Successfully logged in!"
+      session[:server_id] = @server.id
+      check_admin_status
+    else
+      flash[:failure] = "Password incorrect!"
+      render :new
+    end
+  end
+
+  def check_admin_status
+    if @server.admin?
+      redirect_to admin_dashboard_path
+    else
+      redirect_to dashboard_path
+    end
+  end
+
 end

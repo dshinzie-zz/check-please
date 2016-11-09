aee2650501cda65d068e4f6aa9459e21216b9892
Daniel Shin <shin.daniel.d@gmail.com>
2016-11-08 23:21:26 -0700

    add edit item view, controller methods and style

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -14,15 +14,30 @@ class Admin::ItemsController < ApplicationController
       render action: 'new'
     end
   end
-  
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+
+    if @item.update(item_params)
+      flash[:success] = "Item has been updated!"
+      redirect_to admin_dashboard_path
+    else
+      render :edit
+    end
+  end
+
 private
 
     def require_admin
       render file: '/public/404' unless current_admin?
     end
-    
+
     def item_params
       params.require(:item).permit(:name, :avatar, :price)
     end
-  
+
 end
